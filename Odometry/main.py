864afa726f82a55e3f69a611b18a5a2ae6cb6326
Erick Soares Dias <soares8erick@gmail.com>
2023-07-31 11:45:12 -0300

    ajustes

--- Odometry/main.py
+++ Odometry/main.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
 # ---------- Importa as bibliotecas necessarias
-import time
-import math
-from ev3dev2.motor import *
-from ev3dev2.sound import Sound
-from ev3dev2.button import Button
-from ev3dev2.sensor import *
-from ev3dev2.sensor.lego import *
-from ev3dev2.sensor.virtual import *
-from odometry import Odometry
-from utils import *
+import time # importando o tempo para a logica de programacao
+import math # importando a matematica para a logica de programação
+from ev3dev2.motor import * # importando tudo da biblioteca ev3dev2.motor
+from ev3dev2.sound import Sound # importando o som da biblioteca ev3dev2.sound
+from ev3dev2.button import Button # importando os botoes da biblioteca ev3dev2.button
+from ev3dev2.sensor import * # importando tudo da biblioteca ev3dev2.sensor
+from ev3dev2.sensor.lego import *  # importando tudo da biblioteca ev3dev2.sensor.lego
+from ev3dev2.sensor.virtual import * # importando tudo da biblioteca ev3dev2.sensor.virtual
+from odometry import Odometry # importando tudo da biblioteca odometry
+from utils import * # importando tudo da biblioteca utils
+#from robot import *
+from time import sleep #importando sleep da biblioteca time
 
-from time import sleep
 
-
-robot = Robot(OUTPUT_A,OUTPUT_B)
+robot = Robot(OUTPUT_A,OUTPUT_B) # setando o comando robot para utilizar  as saidas OUTPU_A e OUTPUT_B
 pos = Odometry(OUTPUT_A,OUTPUT_B,5.6,15.2,360,360,debug=True)
 robot._gyro = GyroSensor(INPUT_3)
 cont = 0
