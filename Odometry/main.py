bfe36a314b11a15ca7bfc10297ee34c5b6201870
Erick Soares Dias <soares8erick@gmail.com>
2023-07-31 12:19:46 -0300

    ajustes

--- Odometry/main.py
+++ Odometry/main.py
@@ -18,11 +18,27 @@ from time import sleep #importando sleep da biblioteca time
 robot = Robot(OUTPUT_A,OUTPUT_B) # setando o comando robot para utilizar  as saidas OUTPU_A e OUTPUT_B
 pos = Odometry(OUTPUT_A,OUTPUT_B,5.6,15.2,360,360,debug=True)
 
-robot._gyro = GyroSensor(INPUT_4)
+# ---------- Configura os sensores
+
+InfraredSensor.mode = 'IR-PROX' # modo do sensor de infravermelho por aproximação
+
+# ---------- Sensores do primeiro Brick
+color_sensor_in1 = ColorSensor(INPUT_1)  # setando sensor de cor na entrada in1
+color_sensor_in1.mode = 'COL-COLOR' # modo do sensor de cor RGB
+
+color_sensor_in2 = ColorSensor(INPUT_2)  # setando sensor de cor na entrada in2
+color_sensor_in2.mode = 'COL-COLOR' # modo do sensor de cor RGB
+
+ultrasonic_sensor_in3 = UltrasonicSensor(INPUT_3)  # setando sensor ultrassonico na entrada in3
+ultrasonic_sensor_in3.mode = 'US-DIST-CM' # modo do sensor ultrasonico em cm
+
+robot._gyro = GyroSensor(INPUT_4) # setando o sensor de giro na entra in4
+_gyro = 'GYRO-ANG' # modo do sensor gyroscopio em angulhos
 
 cont = 0
 pen = Pen(INPUT_5)
 pen.down()
+
 while True:
     print(pos.get_pos())
     direction = 90 * cont
